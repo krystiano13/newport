745c71c5999dab2fa82f1b2bb2fbc6a042a7f8b0
krystiano13 <crystianotv@gmail.com>
2023-07-22 09:36:50 +0200

    configure gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { src, watch, dest, series } = require("gulp");
+
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const minCss = require("gulp-clean-css");
+
+const styles = () => {
+  return src("./src/sass/*.scss")
+    .pipe(sass())
+    .pipe(concat("app.css"))
+    .pipe(minCss())
+    .pipe(dest("./src/css/"));
+};
+
+const watcher = () => watch(["./src/sass/*.scss"], styles);
+
+exports.default = series(watcher, styles);
