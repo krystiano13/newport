cf396cb212b2cfa9bc4940d437e2b8f9ec371b29
krystiano13 <crystianotv@gmail.com>
2023-07-23 10:27:48 +0200

    Blog page

---
+++ src/components/BlogData.ts
@@ -0,0 +1,22 @@
+export const BlogData = [
+  {
+    id: 0,
+    title: "London",
+    text: "Nunc at orci aliquet, ornare ex at, dignissim lacus. Aenean ipsum justo, maximus eget maximus vel, efficitur ut dui. Donec sit amet mi non tellus tempus tempor et eget diam. Proin mollis suscipit urna, at facilisis sapien pellentesque sed. Maecenas at risus ultrices, tempus libero non, lacinia turpis",
+  },
+  {
+    id: 1,
+    title: "Traveler",
+    text: "Maecenas aliquet augue sed ligula eleifend, ut imperdiet sapien luctus. Aliquam dui tellus, auctor feugiat dui nec, malesuada viverra velit. Ut tellus ipsum, congue et ipsum a, rhoncus congue tortor.",
+  },
+  {
+    id: 2,
+    title: "Hipster",
+    text: "Vivamus a neque nec orci tincidunt laoreet. Suspendisse libero odio, euismod vulputate placerat sit amet, ornare eu nunc. Nulla facilisi. Ut lorem quam, tempus nec elit ac, aliquet lobortis justo. Curabitur tincidunt dolor quis augue lacinia, in pellentesque felis aliquam.",
+  },
+  {
+    id: 3,
+    title: "Modern Workplace",
+    text: "Quisque sit amet egestas lectus, sagittis ornare ipsum. Nulla sed nibh ac felis laoreet luctus. Phasellus in ornare turpis, at aliquam nulla. Mauris pellentesque augue scelerisque, eleifend libero at, feugiat ligula. Nulla ligula erat, fringilla eu convallis eu, vehicula vel risus.",
+  },
+];
--- src/views/Blog.tsx
+++ src/views/Blog.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 
+import { BlogData } from "../components/BlogData";
+
 export const Blog = () => {
-  return <h1>Blog</h1>;
+  return (
+    <main className="Blog">
+      <div className="container-lg">
+        <div className="row">
+          {BlogData.map((item) => (
+            <div
+              key={item.id}
+              className="col-12 d-flex justify-content-between"
+            >
+              <div className="title">
+                <h2>{item.title}</h2>
+                <p>JUNE 26 2020</p>
+              </div>
+              <article>{item.text}</article>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 };
